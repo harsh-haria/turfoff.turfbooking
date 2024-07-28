70edea02765be68d67ea77b06eaa799300aa110d
Harsh Haria <harshharia1234@gmail.com>
2024-07-28 23:42:23 +0530

    Removed unwanted dependencies and env variables from the SecurityConfig file.

--- src/main/java/com/turfoff/turfbooking/config/SecurityConfig.java
+++ src/main/java/com/turfoff/turfbooking/config/SecurityConfig.java
@@ -1,13 +1,10 @@
 package com.turfoff.turfbooking.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -16,12 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    @Value("${com.turfoff.turfbooking.api-key.key}")
-    private String principalRequestHeader;
-
-    @Value("${com.turfoff.turfbooking.api-key.value}")
-    private String principalRequestValue;
 
     @Bean
     public UserDetailsService userDetailsService() {

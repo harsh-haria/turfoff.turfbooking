da147b44f8d393b5697440603808fab789805708
Harsh Haria <harshharia1234@gmail.com>
2024-08-16 19:40:56 +0530

    test added for checking if the turf creation works as expected.
    Signed-off-by: Harsh Haria <harshharia1234@gmail.com>

--- src/test/java/com/turfoff/turfbooking/TestDataUtil.java
+++ src/test/java/com/turfoff/turfbooking/TestDataUtil.java
@@ -2,8 +2,8 @@ package com.turfoff.turfbooking;
 
 import com.turfoff.turfbooking.domain.dto.AdminDto;
 import com.turfoff.turfbooking.domain.entities.AdminEntity;
-
-import java.util.UUID;
+import com.turfoff.turfbooking.domain.entities.TurfEntity;
+import com.turfoff.turfbooking.utilities.TurfStatus;
 
 public class TestDataUtil {
 
@@ -11,7 +11,6 @@ public class TestDataUtil {
 
     public static AdminEntity createTestAdminA () {
         return AdminEntity.builder()
-                .id(1L)
                 .firstName("Harsh")
                 .lastName("Haria")
                 .email("test@harsh.com")
@@ -28,6 +27,25 @@ public class TestDataUtil {
                 .email("test@harsh.com")
                 .password("harshharia")
                 .phone("8181800000")
+                .build();
+    }
+
+    public static TurfEntity createTestTurfEntity(Long adminId) {
+
+        AdminEntity admin = createTestAdminA();
+        admin.setId(adminId);
+
+        return TurfEntity.builder()
+                .id(1L)
+                .name("Test Turf")
+                .status(TurfStatus.ACTIVE)
+                .owner(admin)
+                .manager(admin)
+                .rent(1500)
+                .amenities("AM1, AM2, AM3")
+                .phone("12345 67890")
+                .email("turf1@turfoff.com")
+                .address("Mumbai, In")
                 .build();
     }
 
---
+++ src/test/java/com/turfoff/turfbooking/repositories/TurfEntityRepositoryIntegrationTests.java
@@ -0,0 +1,42 @@
+package com.turfoff.turfbooking.repositories;
+
+import com.turfoff.turfbooking.TestDataUtil;
+import com.turfoff.turfbooking.domain.entities.AdminEntity;
+import com.turfoff.turfbooking.domain.entities.TurfEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class TurfEntityRepositoryIntegrationTests {
+
+    private TurfRepository underTest;
+    private AdminRepository adminRepository;
+
+    @Autowired
+    public TurfEntityRepositoryIntegrationTests(TurfRepository underTest, AdminRepository adminRepository) {
+        this.underTest = underTest;
+        this.adminRepository = adminRepository;
+    }
+
+    @Test
+    public void testThatTurfCanBeCreatedAndRecalled() {
+
+        AdminEntity adminEntity = TestDataUtil.createTestAdminA();
+        AdminEntity savedAdminEntity = adminRepository.save(adminEntity);
+
+        TurfEntity turfEntity = TestDataUtil.createTestTurfEntity(savedAdminEntity.getId());
+        TurfEntity savedEntity = underTest.save(turfEntity);
+        Optional<TurfEntity> receivedTurf = underTest.findById(savedEntity.getId());
+        Assertions.assertThat(receivedTurf).isPresent();
+        Assertions.assertThat(receivedTurf.get()).isEqualTo(turfEntity);
+    }
+}

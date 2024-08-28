9cd7371b5496b4bd91a2ce532902527a2813dd09
Harsh Haria <harshharia1234@gmail.com>
2024-08-28 23:59:59 +0530

    added new services for user
    Signed-off-by: Harsh Haria <harshharia1234@gmail.com>

--- src/main/java/com/turfoff/turfbooking/services/UserService.java
+++ src/main/java/com/turfoff/turfbooking/services/UserService.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 public interface UserService {
     UserEntity saveUser(UserEntity user);
     Optional<UserEntity> getUserById(long id);
+    void updatePassword(String username, String newPassword);
+    void updateEmail(String username, String newEmail);
 }

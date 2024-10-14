d19d80a368ab8142139fc48638bcaf4af376f849
Harsh Haria <harshharia1234@gmail.com>
2024-10-15 02:28:05 +0530

    updated the dependencies in the UserEntity.java file left out in the merge.
    Signed-off-by: Harsh Haria <harshharia1234@gmail.com>

--- src/main/java/com/turfoff/turfbooking/domain/mysql/entities/UserEntity.java
+++ src/main/java/com/turfoff/turfbooking/domain/mysql/entities/UserEntity.java
@@ -1,10 +1,13 @@
 package com.turfoff.turfbooking.domain.mysql.entities;
 
+import com.turfoff.turfbooking.utilities.Roles;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 
 @Data
